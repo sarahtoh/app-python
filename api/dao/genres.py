9e63a7beeaa36cffcbc76ac84d06934f29a1097e
sarahtoh <88479824+sarahtoh@users.noreply.github.com>
2023-12-10 18:04:04 +0800

    Update genres.py based on neo4j academy course

--- api/dao/genres.py
+++ api/dao/genres.py
@@ -25,11 +25,34 @@ class GenreDAO:
     """
     # tag::all[]
     def all(self):
-        # TODO: Open a new session
-        # TODO: Define a unit of work to Get a list of Genres
-        # TODO: Execute within a Read Transaction
+        # Define a unit of work to Get a list of Genres
+        def get_movies(tx):
+            result = tx.run("""
+                MATCH (g:Genre)
+                WHERE g.name <> '(no genres listed)'
 
-        return genres
+                CALL {
+                WITH g
+                MATCH (g)<-[:IN_GENRE]-(m:Movie)
+                WHERE m.imdbRating IS NOT NULL AND m.poster IS NOT NULL
+                RETURN m.poster AS poster
+                ORDER BY m.imdbRating DESC LIMIT 1
+                }
+
+                RETURN g {
+                .*,
+                movies: count { (g)<-[:IN_GENRE]-(:Movie) },
+                poster: poster
+                }
+                ORDER BY g.name ASC
+            """)
+
+            return [ g.value(0) for g in result ]
+
+        # Open a new session
+        with self.driver.session() as session:
+            # Execute within a Read Transaction
+            return session.execute_read(get_movies)
     # end::all[]
 
 
@@ -41,9 +64,31 @@ class GenreDAO:
     """
     # tag::find[]
     def find(self, name):
-        # TODO: Open a new session
-        # TODO: Define a unit of work to find the genre by it's name
-        # TODO: Execute within a Read Transaction
+        # Define a unit of work to find the genre by it's name
+        def find_genre(tx, name):
+            first = tx.run("""
+                MATCH (g:Genre {name: $name})<-[:IN_GENRE]-(m:Movie)
+                WHERE m.imdbRating IS NOT NULL AND m.poster IS NOT NULL AND g.name <> '(no genres listed)'
+                WITH g, m
+                ORDER BY m.imdbRating DESC
 
-        return [g for g in genres if g["name"] == name][0]
+                WITH g, head(collect(m)) AS movie
+
+                RETURN g {
+                    .name,
+                    movies: count { (g)<-[:IN_GENRE]-() },
+                    poster: movie.poster
+                } AS genre
+            """, name=name).single()
+
+            # If no records are found raise a NotFoundException
+            if first == None:
+                raise NotFoundException()
+
+            return first.get("genre")
+
+        # Open a new session
+        with self.driver.session() as session:
+            # Execute within a Read Transaction
+            return session.execute_read(find_genre, name)
     # end::find[]
